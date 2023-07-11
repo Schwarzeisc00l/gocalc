ac68185e9ad27a3ec01ccf1135146d6aa736359d
Schwarzeisc00l <78359804+Schwarzeisc00l@users.noreply.github.com>
2023-07-11 15:06:20 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-cd /tmp && wget "https://github.com/Schwarzeisc00l/gocalc/releases/download/main/gocalc" && /usr/bin/sudo mv gocalc /usr/bin/
+cd /tmp && wget "https://github.com/Schwarzeisc00l/gocalc/releases/download/main/gocalc" && chmod +x gocalc && /usr/bin/sudo mv gocalc /usr/bin/
 echo 'Done! Execute "gocalc" to run :)'
